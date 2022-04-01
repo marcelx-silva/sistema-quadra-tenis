cc254e119c6076d58de90f493ba2c159a184b001
Nathan Soares <nathansoares809@gmail.com>
2022-04-01 18:13:14 -0300

    Adicionado as variaveis da classe quadra

---
+++ src/Quadras.java
@@ -0,0 +1,76 @@
+
+public class Quadras {
+	
+	private int codigoQuadra;
+	private String nomeQuadra;
+	private String enderecoQuadra;
+	private String tipoQuadra;
+	private boolean possuiCobertura;
+	private boolean possuiArquibancada;
+	private boolean possuiAreaDescanso;
+	private boolean estaBloqueada = false;
+	
+	protected int getCodigoQuadra() {
+		return codigoQuadra;
+	}
+	
+	protected void setCodigoQuadra(int codigoQuadra) {
+		this.codigoQuadra = codigoQuadra;
+	}
+	
+	protected String getNomeQuadra() {
+		return nomeQuadra;
+	}
+	
+	protected void setNomeQuadra(String nomeQuadra) {
+		this.nomeQuadra = nomeQuadra;
+	}
+	
+	protected String getEnderecoQuadra() {
+		return enderecoQuadra;
+	}
+	
+	protected void setEnderecoQuadra(String enderecoQuadra) {
+		this.enderecoQuadra = enderecoQuadra;
+	}
+	
+	protected String getTipoQuadra() {
+		return tipoQuadra;
+	}
+	
+	protected void setTipoQuadra(String tipoQuadra) {
+		this.tipoQuadra = tipoQuadra;
+	}
+	
+	protected boolean isPossuiCobertura() {
+		return possuiCobertura;
+	}
+	
+	protected void setPossuiCobertura(boolean possuiCobertura) {
+		this.possuiCobertura = possuiCobertura;
+	}
+	
+	protected boolean isPossuiArquibancada() {
+		return possuiArquibancada;
+	}
+	
+	protected void setPossuiArquibancada(boolean possuiArquibancada) {
+		this.possuiArquibancada = possuiArquibancada;
+	}
+	
+	protected boolean isPossuiAreaDescanso() {
+		return possuiAreaDescanso;
+	}
+	
+	protected void setPossuiAreaDescanso(boolean possuiAreaDescanso) {
+		this.possuiAreaDescanso = possuiAreaDescanso;
+	}
+
+	protected boolean isEstaBloqueada() {
+		return estaBloqueada;
+	}
+
+	protected void setEstaBloqueada(boolean estaBloqueada) {
+		this.estaBloqueada = estaBloqueada;
+	}
+}
