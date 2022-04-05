73c56a5b084f5e5d7af0ed4543dfe2ad5198624e
Nathan Soares <nathansoares809@gmail.com>
2022-04-05 20:31:04 -0300

    Corrigido um erro no uso do Scanner

--- Interfaces/interfaceQuadra.java
+++ Interfaces/interfaceQuadra.java
@@ -5,6 +5,40 @@ public class interfaceQuadra {
 	Scanner scanner = new Scanner(System.in);
 	UtilidadesSimplificadas utilidades = new UtilidadesSimplificadas();
 	
+	final void interfacePrincipalQuadras() {
+		Quadras quadra = new Quadras();
+		int operadorDoMenu = 0;
+		do {
+			utilidades.exibeMensagem("MENU QUADRA"
+					+ "\n1.CADASTRAR QUADRA"
+					+ "\n2.DESBLOQUEAR/BLOQUEAR QUADRAS"
+					+ "\n3.ALTERAR DADOS"
+					+ "\n4.VISUALIZAR QUADRAS"
+					+ "\n5.SAIR DO MENU"
+					+ "\nOPÇÃO: ");
+			
+			operadorDoMenu = scanner.nextInt();
+			scanner.nextLine();
+			
+			switch(operadorDoMenu){
+				
+				case 1:
+					this.interfaceCadastrarQuadra(quadra);
+					break;
+					
+				case 5:
+					utilidades.exibeMensagem("Voltando ao menu anterior!\n");
+					break;
+					
+				default:
+					utilidades.exibeMensagem("Ainda em desenvolvimento!\n");
+					break;
+			}
+			
+		}while(operadorDoMenu != 5);
+		
+	}
+	
 	void interfaceCadastrarQuadra(Quadras quadra) {
 
 		String nome = "";
@@ -16,15 +50,16 @@ public class interfaceQuadra {
 		boolean area = false;
 		
 		try {
+			
 			utilidades.exibeMensagem("Nome da quadra: ");
 			nome = scanner.nextLine();
 			
+			utilidades.exibeMensagem("Número da quadra: ");
+			numero = Integer.parseInt(scanner.nextLine());
+			
 			utilidades.exibeMensagem("Endereço da quadra: ");
 			endereco = scanner.nextLine();
-			
-			utilidades.exibeMensagem("Número da quadra: ");
-			numero = scanner.nextInt();
-			
+		
 			utilidades.exibeMensagem("Tipo da quadra, selecione uma das opções atráves dos números:"
 									+ "\n1. SAIBRO"
 									+ "\n2. SUPERFÍCIE SINTÉTICA"
@@ -42,9 +77,10 @@ public class interfaceQuadra {
 			utilidades.exibeMensagem("Possui área de descanso: ");
 			area = utilidades.transformaString(utilidades.persistirValor(scanner.next()));
 			
+			utilidades.exibeMensagem("Cadastro Realizado com Sucesso!\n");
 			quadra.cadastraQuadra(numero, nome, endereco, tipo, cobertura, arquibancada, area);
 		}catch(Exception e) {
-			utilidades.exibeMensagem("Cadastro interrompido!");
+			utilidades.exibeMensagem("Cadastro interrompido!\n");
 		}
 	}
 }
