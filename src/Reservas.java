1c139a78790d4130120f14da422cb8c942310ef2
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2022-04-06 17:16:41 -0300

    Adicionado variáveis e getters/setters

--- src/Reservas.java
+++ src/Reservas.java
@@ -1,3 +1,70 @@
+
 public class Reservas {
+	
+	
+	
+	private String nome_reservista;
+	
+	private String data_reserva;
+	private String hr_inicio_reserva;
+	private String hr_fim_reserva;
+	
+	private int modo_pagamento;
+	private int parcelas; 
+	
+	
+	
+	
+	protected String getNome_reservista() {
+		return nome_reservista;
+	}
+	
+	protected String getData_reserva(){
+		return data_reserva;
+	}
+	
+	protected String getHr_inicio_reserva() {
+		return hr_inicio_reserva;
+	}
+	
+	protected String getHr_fim_reserva() {
+		return hr_fim_reserva;
+	}
+	
+	protected int getModo_pagamento() {
+		return modo_pagamento;
+	}
+	
+	protected int getParcelas(){
+		return parcelas;
+	}
+	
+	protected void setNome_reservista(String nome_reservista){
+		this.nome_reservista = nome_reservista;
+	}
+	
+	protected void setData_reserva(String data_reserva){
+		this.data_reserva = data_reserva;
+	}
+	
+	protected void setHr_inicio_reserva(String hr_inicio_reserva){
+		this.hr_inicio_reserva = hr_inicio_reserva;
+	}
+	
+	protected void setHr_fim_reserva(String hr_fim_reserva){
+		this.hr_fim_reserva = hr_fim_reserva;
+	}
+	
+	protected void setModo_pagamento(int modo_pagamento){
+		this.modo_pagamento = modo_pagamento;
+	}
+	
+	protected void setParcelas(int parcelas){
+		this.parcelas = parcelas;
+	}
+	
+	
+	
+	
 	
 }
