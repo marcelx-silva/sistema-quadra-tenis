7e8db755664ea95cea358428bc241bfb65afa8a0
Thiago <thiago.silvamarcelo56@gmail.com>
2022-05-12 21:56:18 -0300

    feat: adicionado classe para a conexao com o banco de dados

---
+++ DAO/Conexao/ConexaoBD.java
@@ -0,0 +1,34 @@
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//import com.mysql.cj.jdbc.Driver;
+
+public class ConexaoBD {
+	
+	public Connection conectaBD() {
+		
+		Connection c = null;
+		
+		try {
+			
+			String urlConexao = "";
+			String usuAdmin = "";
+			String senhaAdmin = "";
+			
+			//Driver driver = new Driver();
+			//DriverManager.registerDriver(driver);
+			
+			c = DriverManager.getConnection(urlConexao,usuAdmin,senhaAdmin);
+		
+			
+		
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return (c);
+	}
+}
