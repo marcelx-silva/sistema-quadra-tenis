2fc89d2c0fad4e0c966ed70e22a8e613f4492d9e
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-05-14 18:07:30 -0300

    test: terminados os testes básicos da HU11

--- Testes/TestesDeUsuario.java
+++ Testes/TestesDeUsuario.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 class TestesDeUsuario {
 	
-	Usuario usu = new Usuario("Nathan", "132.456.879-10", "emailDoNathan@gmail.com", "senha segura", false, true, false, false);
+	Usuario usu = new Usuario("Nathan", "132.456.879-10", "emailDoNathan@gmail.com", "senha segura", false, false, false, false);
 
 	@Test
 	void testeDeCadastroUsuario() {	
@@ -14,7 +14,7 @@ class TestesDeUsuario {
 		assertEquals("emailDoNathan@gmail.com", usu.getEmailUsuario());
 		assertEquals("senha segura", usu.getSenhaUsuario());
 		assertFalse(usu.isAcessoGestorQuadras());
-		assertTrue(usu.isAcessoGestorUsuarios());
+		assertFalse(usu.isAcessoGestorUsuarios());
 		assertFalse(usu.isAcessoRelatorios());
 		assertFalse(usu.isAcessoZelador());
 	}	
@@ -63,5 +63,53 @@ class TestesDeUsuario {
 	void testeMudarSenhaUsuario() {
 		usu.alteraUsuario("senha mais segura ainda", 4);
 		assertEquals("senha mais segura ainda", usu.getSenhaUsuario());
+	}
+	
+	@Test
+	void darAcessoGestorQuadras() {
+		usu.alteraUsuario("sim", 5);
+		assertTrue(usu.isAcessoGestorQuadras());
+	}
+	
+	@Test
+	void retirarAcessoGestorQuadras() {
+		usu.alteraUsuario("não", 5);
+		assertFalse(usu.isAcessoGestorQuadras());
+	}
+	
+	@Test
+	void darAcessoGestorUsuario() {
+		usu.alteraUsuario("sim", 6);
+		assertTrue(usu.isAcessoGestorUsuarios());
+	}
+	
+	@Test
+	void retirarAcessoGestorUsuario() {
+		usu.alteraUsuario("não", 6);
+		assertFalse(usu.isAcessoGestorUsuarios());
+	}
+	
+	@Test
+	void darAcessoRelatorio() {
+		usu.alteraUsuario("sim", 7);
+		assertTrue(usu.isAcessoRelatorios());
+	}
+	
+	@Test
+	void retirarAcessoRelatorio() {
+		usu.alteraUsuario("não", 7);
+		assertFalse(usu.isAcessoRelatorios());
+	}
+	
+	@Test
+	void darAcessoZelador() {
+		usu.alteraUsuario("sim", 8);
+		assertTrue(usu.isAcessoZelador());
+	}
+	
+	@Test
+	void retirarAcessoZelador() {
+		usu.alteraUsuario("não", 8);
+		assertFalse(usu.isAcessoZelador());
 	}
 }
