0210ffd97f45229b4bbe49f7b9435e8c26c9c53a
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-05-20 10:02:13 -0300

    feat: criado a logica do construtor de agendamento

--- src/Agendamento.java
+++ src/Agendamento.java
@@ -12,4 +12,45 @@ public class Agendamento {
 	public Agendamento(Quadra q, Cliente c, LocalDate data, LocalTime horarioInicio, LocalTime horarioFim) {
 		
 	}
+
+	protected Quadra getQuadra() {
+		return quadra;
+	}
+
+	protected void setQuadra(Quadra quadra) {
+		this.quadra = quadra;
+	}
+
+	protected Cliente getCliente() {
+		return cliente;
+	}
+
+	protected void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	protected LocalDate getData() {
+		return data;
+	}
+
+	protected void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	protected LocalTime getHorarioInicio() {
+		return horarioInicio;
+	}
+
+	protected void setHorarioInicio(LocalTime horarioInicio) {
+		this.horarioInicio = horarioInicio;
+	}
+
+	protected LocalTime getHorarioFim() {
+		return horarioFim;
+	}
+
+	protected void setHorarioFim(LocalTime horarioFim) {
+		this.horarioFim = horarioFim;
+	}
+	
 }
