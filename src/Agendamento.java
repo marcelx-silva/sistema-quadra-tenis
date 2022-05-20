ea0c4b523983a08839ed9a5abb84556ab1f1b9dd
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-05-20 10:01:05 -0300

    feat: criado estrutura inicial de agendamento

--- src/Agendamento.java
+++ src/Agendamento.java
@@ -1,4 +1,15 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Agendamento {
 
+	private Quadra quadra;
+	private Cliente cliente;
+	private LocalDate data;
+	private LocalTime horarioInicio;
+	private LocalTime horarioFim;
+	
+	public Agendamento(Quadra q, Cliente c, LocalDate data, LocalTime horarioInicio, LocalTime horarioFim) {
+		
+	}
 }
--- src/Manutencao.java
+++ src/Manutencao.java
@@ -1,4 +1,5 @@
 
-public class Manuntecao {
+public class Manutencao {
 
+	
 }
