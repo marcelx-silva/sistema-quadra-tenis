caedca1690a35c8ed22427138fbe03a6c3963f7b
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-05-25 16:53:25 -0300

    feat: criadas as DMLs de usuario

--- DAO/queries/QueriesUsuario.java
+++ DAO/queries/QueriesUsuario.java
@@ -63,5 +63,28 @@ public class QueriesUsuario {
 		queriesUsuario.store(out, null);
 		out.close();
 	}
+	
+	protected void DMLUsuario() throws IOException{
+		
+		queriesUsuario.setProperty("INSERT_INTO_USUARIO", "INSERT INTO usuario(usu_nome, usu_senha, usu_email, usu_dt_cadastro) VALUES (?,?,?,?);");
+		
+		queriesUsuario.setProperty("UPDATE_USU_NOME", "UPDATE usuario SET usu_nome = ? WHERE usu_id = ?;");
+		queriesUsuario.setProperty("UPDATE_USU_EMAIL", "UPDATE usuario SET usu_email = ? WHERE usu_id = ?;");
+		queriesUsuario.setProperty("UPDATE_USU_SENHA", "UPDATE usuario SET usu_senha = ? WHERE usu_id = ?;");
+		
+		queriesUsuario.setProperty("BLOCK_USU_BY_ID", "UPDATE usuario SET usu_bloqueado = ? WHERE usu_id = ?");
+		queriesUsuario.setProperty("DISABLE_USU_BY_ID", "UPDATE usuario SET usu_habilitado = ? WHERE usu_id = ?");
+		
+		queriesUsuario.setProperty("UPDATE_USU_ACESSO_GESTOR_QUADRA", "UPDATE usuario SET usu_acesso_gestor_quadra = ? WHERE usu_id = ?;");
+		queriesUsuario.setProperty("UPDATE_USU_ACESSO_GESTOR_USUARIO", "UPDATE usuario SET usu_acesso_gestor_usuario = ? WHERE usu_id = ?;");
+		queriesUsuario.setProperty("UPDATE_USU_ACESSO_RELATORIO", "UPDATE usuario SET usu_acesso_relatorio = ? WHERE usu_id = ?");
+		queriesUsuario.setProperty("UPDATE_USU_ACESSO_ZELADOR", "UPDATE usuario SET usu_acesso_zelador = ? WHERE usu_us = ?");
+		
+		queriesUsuario.setProperty("DELETE_USUARIO_BY_ID", "DELETE FROM usuario WHERE usu_id = ?");
+		
+		FileOutputStream out = new FileOutputStream("DML_USUARIO.properties");
+		queriesUsuario.store(out, null);
+		out.close();
+	}
 
 }
