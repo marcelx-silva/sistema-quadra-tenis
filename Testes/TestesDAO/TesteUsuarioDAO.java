b45bb97b2ad5521699891db88616e789dc8e75af
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-05-25 18:26:55 -0300

    Test: feito o teste do cadastro de usuario no banco

---
+++ Testes/TestesDAO/TesteUsuarioDAO.java
@@ -0,0 +1,20 @@
+package TestesDAO;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import implementacaoDAO.ImplUsuarioDAO;
+import Dominio.Usuario;
+
+public class TesteUsuarioDAO {
+
+	Usuario usu = new Usuario("Nathan", "132.456.879-10", "emailDoNathan@gmail.com", "senha segura", false, false, false, false);
+	ImplUsuarioDAO usuDAO = new ImplUsuarioDAO();
+	
+	@Test
+	public void TesteCadastroUsuario() {
+		assertTrue(usuDAO.CadastrarUsuario(usu));
+	}
+
+}
--- Testes/TestesDominio/TesteUsuario.java
+++ Testes/TestesDominio/TesteUsuario.java
@@ -1,3 +1,4 @@
+package TestesDominio;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
