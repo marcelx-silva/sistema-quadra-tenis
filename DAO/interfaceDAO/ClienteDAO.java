b428d76fad13ae30e34289db6d3961ba8d8cb162
Thiago <thiago.silvamarcelo56@gmail.com>
2022-06-10 02:55:18 -0300

    refactor: modificado parametros dos metodos

--- DAO/interfaceDAO/ClienteDAO.java
+++ DAO/interfaceDAO/ClienteDAO.java
@@ -10,28 +10,21 @@ import java.util.List;
 public interface ClienteDAO {
 	
 	
-	//Seleciona todos os clientes
 	List<Cliente> obterTodosClientes() throws IOException, SQLException;
 	
-	//Seleciona cliente pelo identificador
 	Cliente obterClientePeloId(int id) throws ClientNotFoundException;
 	
-	//Seleciona clientes bloqueados ou desbloqueados
 	List<Cliente> obterClienteHabilitados(boolean bloqueado) throws IOException, SQLException;
 		
-	//Cadastra clintes
 	boolean CadastrarCliente(Cliente c);
 	
-	//Atualizar Dados do Cliente
 	boolean AlterarDadosCliente(Cliente c,String alteracao, int escolha);
 	
-	//Bloquear e Desbloquear Cliente
-	boolean DesabilitarCliente(Cliente c, boolean habilitado)throws ClientNotFoundException ;
+	boolean DesabilitarCliente(String cpf, boolean habilitado)throws ClientNotFoundException ;
 	
-	boolean BloquearCliente(Cliente c, boolean bloqueado) throws  ClientNotFoundException;
+	boolean BloquearCliente(String cpf, boolean bloqueado) throws  ClientNotFoundException;
 	
-	boolean InvalidarCliente(Cliente c, boolean validado) throws  ClientNotFoundException;
+	boolean InvalidarCliente(String cpf, boolean validado) throws  ClientNotFoundException;
 	
-	//Deletar cliente
-	boolean DeletarCliente(int id) throws  ClientNotFoundException;
+	boolean DeletarCliente(String cpf) throws  ClientNotFoundException;
 }
