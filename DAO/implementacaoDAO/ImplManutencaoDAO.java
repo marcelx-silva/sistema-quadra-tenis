d732b6a6f1b0fff6039c4e8566e7658da765d2c0
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-06-10 19:31:03 -0300

    refactor: adaptado o codigo para metodo que monta Objetos de manutenção

--- DAO/implementacaoDAO/ImplManutencaoDAO.java
+++ DAO/implementacaoDAO/ImplManutencaoDAO.java
@@ -15,8 +15,10 @@ import java.sql.ResultSet;
 
 import Dominio.Manutencao;
 import Dominio.Quadra;
+import Enum.TipoQuadra;
 import Exceptions.CourtNotFoundException;
 import Exceptions.MaintenanceNotFoundException;
+import Utilitario.UtilidadesConversao;
 import Utilitario.UtilidadesGUI;
 import conexao.ConexaoBD;
 import interfaceDAO.ManutencaoDAO;
@@ -39,6 +41,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		List<Manutencao> listaManutencoes = new ArrayList();
 		
 		try {
+			qm.consultaManutencao();
 			FileInputStream in = new FileInputStream("QUERY_CONSULTA_MANUTENCAO.properties");
 			qm.queriesManutencao.load(in);
 			in.close();
@@ -47,20 +50,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 			ResultSet rs = stmt.executeQuery();
 			
 			while(rs.next()) {
-				String codigo = rs.getString("m.man_id");
-				String descricao = rs.getString("m.man_desc");
-				String data = rs.getString("m.mand_data");
-				String horarioInicio = rs.getString("m.man_hr_inicio");
-				String horarioFim = rs.getString("m.man_hr_fim");
-				boolean preventiva = rs.getBoolean("m.man_prev");
-				String codigoQuadra = String.valueOf(rs.getInt("m.man_cod_quadra"));
-				
-				Quadra q = quadraDAO.obterQuadraPeloID(codigoQuadra);
-				
-				LocalTime horarioI = LocalTime.parse(horarioInicio, horarioFormatoPadrao);
-				LocalTime horarioF = LocalTime.parse(horarioFim, horarioFormatoPadrao);
-				LocalDate dataM = LocalDate.parse(data, dataFormatoPadrao);
-				Manutencao m = new Manutencao(codigo, q, dataM, horarioI, horarioF, preventiva, descricao);
+				Manutencao m = montaManutencao(rs);
 				listaManutencoes.add(m);
 			}
 			
@@ -70,8 +60,6 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		}catch(SQLException e) {
 			e.printStackTrace();
 			
-		}catch(CourtNotFoundException e) {
-			e.printStackTrace();
 		}
 		
 		return listaManutencoes;
@@ -82,6 +70,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		List<Manutencao> listaManutencoes = new ArrayList();
 		
 		try {
+			qm.consultaManutencao();
 			FileInputStream in = new FileInputStream("QUERY_CONSULTA_MANUTENCAO.properties");
 			qm.queriesManutencao.load(in);
 			in.close();
@@ -92,20 +81,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 			ResultSet rs = stmt.executeQuery();
 			
 			while(rs.next()) {
-				String codigo = rs.getString("m.man_id");
-				String descricao = rs.getString("m.man_desc");
-				String data = rs.getString("m.mand_data");
-				String horarioInicio = rs.getString("m.man_hr_inicio");
-				String horarioFim = rs.getString("m.man_hr_fim");
-				boolean preventiva = rs.getBoolean("m.man_prev");
-				String codigoQuadra = String.valueOf(rs.getInt("m.man_cod_quadra"));
-				
-				Quadra q = quadraDAO.obterQuadraPeloID(codigoQuadra);
-				
-				LocalTime horarioI = LocalTime.parse(horarioInicio, horarioFormatoPadrao);
-				LocalTime horarioF = LocalTime.parse(horarioFim, horarioFormatoPadrao);
-				LocalDate dataM = LocalDate.parse(data, dataFormatoPadrao);
-				Manutencao m = new Manutencao(codigo, q, dataM, horarioI, horarioF, preventiva, descricao);
+				Manutencao m = montaManutencao(rs);
 				listaManutencoes.add(m);
 			}
 			
@@ -115,8 +91,6 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		}catch(SQLException e) {
 			e.printStackTrace();
 			
-		}catch(CourtNotFoundException e) {
-			e.printStackTrace();
 		}
 		
 		return listaManutencoes;
@@ -127,6 +101,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		List<Manutencao> listaManutencoes = new ArrayList();
 		
 		try {
+			qm.consultaManutencao();
 			FileInputStream in = new FileInputStream("QUERY_CONSULTA_MANUTENCAO.properties");
 			qm.queriesManutencao.load(in);
 			in.close();
@@ -136,20 +111,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 			ResultSet rs = stmt.executeQuery();
 			
 			while(rs.next()) {
-				String codigo = rs.getString("m.man_id");
-				String descricao = rs.getString("m.man_desc");
-				String data = rs.getString("m.mand_data");
-				String horarioInicio = rs.getString("m.man_hr_inicio");
-				String horarioFim = rs.getString("m.man_hr_fim");
-				boolean preventiva = rs.getBoolean("m.man_prev");
-				String codigoQuadra = String.valueOf(rs.getInt("m.man_cod_quadra"));
-				
-				Quadra q = quadraDAO.obterQuadraPeloID(codigoQuadra);
-				
-				LocalTime horarioI = LocalTime.parse(horarioInicio, horarioFormatoPadrao);
-				LocalTime horarioF = LocalTime.parse(horarioFim, horarioFormatoPadrao);
-				LocalDate dataM = LocalDate.parse(data, dataFormatoPadrao);
-				Manutencao m = new Manutencao(codigo, q, dataM, horarioI, horarioF, preventiva, descricao);
+				Manutencao m = montaManutencao(rs);
 				listaManutencoes.add(m);
 			}
 			
@@ -159,8 +121,6 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		}catch(SQLException e) {
 			e.printStackTrace();
 			
-		}catch(CourtNotFoundException e) {
-			e.printStackTrace();
 		}
 		
 		return listaManutencoes;
@@ -171,6 +131,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		List<Manutencao> listaManutencoes = new ArrayList();
 		
 		try {
+			qm.consultaManutencao();
 			FileInputStream in = new FileInputStream("QUERY_CONSULTA_MANUTENCAO.properties");
 			qm.queriesManutencao.load(in);
 			in.close();
@@ -182,20 +143,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 			ResultSet rs = stmt.executeQuery();
 			
 			while(rs.next()) {
-				String codigo = rs.getString("m.man_id");
-				String descricao = rs.getString("m.man_desc");
-				String data = rs.getString("m.mand_data");
-				String horarioInicio = rs.getString("m.man_hr_inicio");
-				String horarioFim = rs.getString("m.man_hr_fim");
-				boolean preventiva = rs.getBoolean("m.man_prev");
-				String codigoQuadra = String.valueOf(rs.getInt("m.man_cod_quadra"));
-				
-				Quadra q = quadraDAO.obterQuadraPeloID(codigoQuadra);
-				
-				LocalTime horarioI = LocalTime.parse(horarioInicio, horarioFormatoPadrao);
-				LocalTime horarioF = LocalTime.parse(horarioFim, horarioFormatoPadrao);
-				LocalDate dataM = LocalDate.parse(data, dataFormatoPadrao);
-				Manutencao m = new Manutencao(codigo, q, dataM, horarioI, horarioF, preventiva, descricao);
+				Manutencao m = montaManutencao(rs);
 				listaManutencoes.add(m);
 			}
 			
@@ -205,8 +153,6 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		}catch(SQLException e) {
 			e.printStackTrace();
 			
-		}catch(CourtNotFoundException e) {
-			e.printStackTrace();
 		}
 		
 		return listaManutencoes;
@@ -217,6 +163,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		
 		Manutencao m = null;
 		try {
+			qm.consultaManutencao();
 			FileInputStream in = new FileInputStream("QUERY_CONSULTA_MANUTENCAO.properties");
 			qm.queriesManutencao.load(in);
 			in.close();
@@ -226,20 +173,7 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 			ResultSet rs = stmt.executeQuery();
 			
 			if(rs.next()) {
-				String codigo = rs.getString("m.man_id");
-				String descricao = rs.getString("m.man_desc");
-				String data = rs.getString("m.mand_data");
-				String horarioInicio = rs.getString("m.man_hr_inicio");
-				String horarioFim = rs.getString("m.man_hr_fim");
-				boolean preventiva = rs.getBoolean("m.man_prev");
-				String codigoQuadra = String.valueOf(rs.getInt("m.man_cod_quadra"));
-				
-				Quadra q = quadraDAO.obterQuadraPeloID(codigoQuadra);
-				
-				LocalTime horarioI = LocalTime.parse(horarioInicio, horarioFormatoPadrao);
-				LocalTime horarioF = LocalTime.parse(horarioFim, horarioFormatoPadrao);
-				LocalDate dataM = LocalDate.parse(data, dataFormatoPadrao);
-				m = new Manutencao(codigo, q, dataM, horarioI, horarioF, preventiva, descricao);
+				m = montaManutencao(rs);
 			}else {
 				throw new MaintenanceNotFoundException("Não foi encontrada nenhuma manutenção cadastrada com este código!");
 			}
@@ -250,8 +184,6 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		}catch(SQLException e) {
 			e.printStackTrace();
 			
-		}catch(CourtNotFoundException e) {
-			e.printStackTrace();
 		}
 		
 		return m;
@@ -368,4 +300,20 @@ public class ImplManutencaoDAO implements ManutencaoDAO {
 		return true;
 	}
 
+	public Manutencao montaManutencao(ResultSet rs) throws SQLException {
+		
+		String codigo = rs.getString("m.man_id");
+		String descricao = rs.getString("m.man_desc");
+		String data = rs.getString("m.mand_data");
+		String horarioInicio = rs.getString("m.man_hr_inicio");
+		String horarioFim = rs.getString("m.man_hr_fim");
+		boolean preventiva = rs.getBoolean("m.man_prev");
+		
+		Quadra qua = quadraDAO.montaQuadra(rs);
+		
+		String dataM = LocalDate.parse(data, dataFormatoBD).format(dataFormatoPadrao);
+		horarioInicio = LocalTime.parse(horarioInicio, horarioFormatoBD).format(horarioFormatoPadrao);	
+		horarioFim = LocalTime.parse(horarioFim, horarioFormatoBD).format(horarioFormatoPadrao);
+		return new Manutencao(codigo, qua, LocalDate.parse(dataM, dataFormatoPadrao), LocalTime.parse(horarioInicio, horarioFormatoPadrao), LocalTime.parse(horarioFim, horarioFormatoPadrao), preventiva, descricao);
+	}
 }
--- DAO/queries/QueriesManutencao.java
+++ DAO/queries/QueriesManutencao.java
@@ -11,30 +11,35 @@ public class QueriesManutencao {
 	public void consultaManutencao() throws IOException{
 		
 		queriesManutencao.setProperty("SELECT_ALL_FROM_MANUTENCAO", "SELECT m.man_id, m.man_desc, DATE_FORMAT(m.man_data, \"%d-%m-%Y\") \r\n"
-				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, m.man_cod_quadra "
-				+ "FROM manutencao m JOIN quadra q"
+				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, q.qua_nome, q.qua_endereco, q.qua_id_tipo, q.qua_cobertura, q.qua_arquibancada \r\n"
+				+ "q.qua_area_descando, q.qua_bloqueado \r \n"
+				+ "FROM manutencao m JOIN quadra q \r\n"
 				+ "ON (q.qua_id = m.man_cod_quadra)");
-		
+				
 		queriesManutencao.setProperty("SELECT_ALL_FROM_MANUTENCAO_BY_DAY", "SELECT m.man_id, m.man_desc, DATE_FORMAT(m.man_data, \"%d-%m-%Y\") \r\n"
-				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, m.man_cod_quadra \r\n"
+				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, q.qua_nome, q.qua_endereco, q.qua_id_tipo, q.qua_cobertura, q.qua_arquibancada \r\n"
+				+ "q.qua_area_descando, q.qua_bloqueado \r \n"
 				+ "FROM manutencao m JOIN quadra q \r\n"
 				+ "ON (q.qua_id = m.man_cod_quadra)"
 				+ "WHERE m.man_data = ?");
 		
 		queriesManutencao.setProperty("SELECT_ALL_FROM_MANUTENCAO_BY_DAY_AND_TIME", "SELECT m.man_id, m.man_desc, DATE_FORMAT(m.man_data, \"%d-%m-%Y\") \r\n"
-				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, m.man_cod_quadra \r\n"
+				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, q.qua_nome, q.qua_endereco, q.qua_id_tipo, q.qua_cobertura, q.qua_arquibancada \r\n"
+				+ "q.qua_area_descando, q.qua_bloqueado \r \n"
 				+ "FROM manutencao m JOIN quadra q \r\n"
 				+ "ON (q.qua_id = m.man_cod_quadra)"
 				+ "WHERE m.man_data = ? AND m.man_hr_inicio >= ? AND m.man_hr_fim <= ?");
 		
 		queriesManutencao.setProperty("SELECT_ALL_FROM_MANUTENCAO_BY_INTERVAL_OF_DAYS", "SELECT m.man_id, m.man_desc, DATE_FORMAT(m.man_data, \"%d-%m-%Y\") \r\n"
-				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, m.man_cod_quadra \r\n"
+				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, q.qua_nome, q.qua_endereco, q.qua_id_tipo, q.qua_cobertura, q.qua_arquibancada \r\n"
+				+ "q.qua_area_descando, q.qua_bloqueado \r \n"
 				+ "FROM manutencao m JOIN quadra q \r\n"
 				+ "ON (q.qua_id = m.man_cod_quadra)"
 				+ "WHERE m.man_data >= ? AND m.man_data <= ?");
 		
 		queriesManutencao.setProperty("SELECT_ALL_FROM_MANUTENCAO_BY_ID", "SELECT m.man_id, m.man_desc, DATE_FORMAT(m.man_data, \"%d-%m-%Y\") \r\n"
-				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, m.man_cod_quadra \r\n"
+				+ "m.man_hr_inicio, m.man_hr_fim, m.man_prev, q.qua_nome, q.qua_endereco, q.qua_id_tipo, q.qua_cobertura, q.qua_arquibancada \r\n"
+				+ "q.qua_area_descando, q.qua_bloqueado \r \n"
 				+ "FROM manutencao m JOIN quadra q \r\n"
 				+ "ON (q.qua_id = m.man_cod_quadra)"
 				+ "WHERE m.man_id = ?");
