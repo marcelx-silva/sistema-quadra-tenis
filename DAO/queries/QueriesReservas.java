3ac816963e9afa14e25233e5843802018a00e111
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-06-10 21:47:00 -0300

    refactor: movidas as queries de relatorios para outra classe

--- DAO/queries/QueriesReservas.java
+++ DAO/queries/QueriesReservas.java
@@ -77,21 +77,6 @@ public class QueriesReservas {
 				+ "ON (r.res_id_quadra = q.qua_id)\r\n"
 				+ "WHERE q.qua_id = ? AND r.res_data >= ? AND r.res_data <= ?");
 		
-		queriesReserva.setProperty("RECEIPT_BY_INTERVAL_OF_DAYS", "SELECT AVG(res_valor) 'res_valor'\r\n"
-				+ "FROM reserva\r\n"
-				+ "WHERE res_data >= ? AND res_data <= ?");
-		
-		queriesReserva.setProperty("RECEIPT_BY_DAY", "SELECT AVG(res_valor) 'res_valor'\r\n"
-				+ "FROM reserva\r\n"
-				+ "WHERE res_data = ?");
-		
-		queriesReserva.setProperty("RESERVATION_BY_DAY_OF_WEEK", "SELECT COUNT(r.res_id), q.qua_nome, q.qua_id, q.qua_nome, q.qua_endereco, q.qua_id_tipo,"
-				+ "q.qua_cobertura, q.qua_arquibancada, q.qua_area_descanso, DAYOFWEEK(r.res_data)\r\n"
-				+ "FROM reserva r JOIN quadra q\r\n"
-				+ "ON(r.res_id_quadra = q.qua_id)\r\n"
-				+ "GROUP BY DAYOFWEEK(r.res_data); ");
-			
-		
 		FileOutputStream out = new FileOutputStream("QUERY_RESERVA.properties");
 		queriesReserva.store(out, null);
 		out.close();
